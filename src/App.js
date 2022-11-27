30095a1f65ee2e0714be352d7d2332bc8eba3c28
Ana Carvalho <anapaulacmoura@gmail.com>
2022-11-27 17:17:01 -0300

    correct letter  shows up on the screen after user selection

--- src/App.js
+++ src/App.js
@@ -13,6 +13,7 @@ function App() {
   const [disabled, setDisabled] = useState(true)
   const [imageForca, setImageForca] = useState(0)
   const [word, setWord] = useState([])
+  const [guessWord, setGuessWord] = useState([])
   const [letter, setLetter] = useState((alfabeto))
   const [specialCharacters, setSpecialCharacters] = useState("")
     
@@ -20,6 +21,7 @@ function startGame(){
   setDisabled(false)
   randomWord()
   setLetter([])
+  setImageForca(0)
 }
 
 function randomWord(){
@@ -27,9 +29,14 @@ function randomWord(){
   const wordCount = palavras.length
   const wordPosition = Math.floor(Math.random()* wordCount) 
   const showWord = palavras[wordPosition]
-  const wordToArray = showWord.split("").map(() => " _")
+  const wordToArray = showWord.split("")
   setWord(wordToArray)
   console.log(showWord)
+
+  let underline = []
+  wordToArray.forEach((letter) => underline.push("_ "))
+  setGuessWord(underline);
+  console.log(underline)
 
   const newWord = showWord.normalize('NFD').replace(/[\u0300-\u036f]/g, "")
   setSpecialCharacters(newWord)
@@ -48,9 +55,29 @@ function chosenLetter(l){
 }
 
 function isCorrect(l){
-console.log("essa palavra tem a letra " + l)
+console.log("Passo 1: essa palavra tem a letra " + l)
+const newGuessedWord = [...guessWord]
+console.log("Passo 2: newGuessedWord " + newGuessedWord )
+console.log("Passo 3: specialCharacters " + specialCharacters)
+console.log("Passo 4: word " + word)
+word.forEach((letra, i) => {
+  if (specialCharacters[i] === l){
+    newGuessedWord[i] = letra
+
+    console.log("a letra é " + letra)
+  }
+})
+setGuessWord(newGuessedWord)
 }
 
+// function acertouLetra(l){
+//   const novaPalavraJogo = [...palavraDoJogo]
+//   palavraEscolhida.forEach((letra, i) => {
+//       if(stringSemAcentos[i] === l){
+//           novaPalavraJogo[i] = letra
+//       }
+//   })
+//   setPalavraDoJogo(novaPalavraJogo)
 
 function isWrong(l){
 console.log("essa palavra não tem a letra " + l)
@@ -59,7 +86,7 @@ setImageForca(newForcaImage)
 }
   return (
    <div className="containerJogo">
-    <Jogo startGame={startGame} imageForca={images[imageForca]} word={word}/>
+    <Jogo startGame={startGame} imageForca={images[imageForca]} guessWord={guessWord}/>
     <Letras alfabeto={alfabeto} letter={letter} chosenLetter={chosenLetter} />
     <Chute inputChute={disabled} buttonChute={disabled} />
    </div>
--- src/Jogo.js
+++ src/Jogo.js
@@ -1,6 +1,3 @@
-const images = ["images/forca0.png", "images/forca1.png", "images/forca3.png", "images/forca4.png", "images/forca5.png", "images/forca6.png"]
-
-
 export default function Jogo(props){
     
     return (
@@ -8,7 +5,7 @@ export default function Jogo(props){
             <img src={props.imageForca} alt="forca"/>
             <div className="escolherPalavra">
             <button onClick={props.startGame}>Escolher palavra</button>
-            <p className="palavraCorreta">{props.word}</p>
+            <p className="palavraCorreta">{props.guessWord}</p>
             </div>
         </div>
 
