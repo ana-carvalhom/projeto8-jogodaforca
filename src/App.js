7067a720f3036612d6898ba76f7e5e1ae7e80963
Ana Carvalho <anapaulacmoura@gmail.com>
2022-11-27 16:28:48 -0300

    FForca image changes depending on number of mistakes made

--- src/App.js
+++ src/App.js
@@ -5,16 +5,16 @@ import palavras from "./palavras";
 import { useState } from "react";
 
 const alfabeto = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+const images = ["images/forca0.png", "images/forca1.png", "images/forca3.png", "images/forca4.png", "images/forca5.png", "images/forca6.png"]
 
 
 function App() {
-
-  const images = ["images/forca0.png", "images/forca1.png", "images/forca3.png", "images/forca4.png", "images/forca5.png", "images/forca6.png"]
 
   const [disabled, setDisabled] = useState(true)
   const [imageForca, setImageForca] = useState(0)
   const [word, setWord] = useState([])
   const [letter, setLetter] = useState((alfabeto))
+  const [specialCharacters, setSpecialCharacters] = useState("")
     
 function startGame(){
   setDisabled(false)
@@ -29,14 +29,34 @@ function randomWord(){
   const showWord = palavras[wordPosition]
   const wordToArray = showWord.split("").map(() => " _")
   setWord(wordToArray)
+  console.log(showWord)
+
+  const newWord = showWord.normalize('NFD').replace(/[\u0300-\u036f]/g, "")
+  setSpecialCharacters(newWord)
 
 }
 
 function chosenLetter(l){
   setLetter([...letter, l])
   console.log(letter)
+
+  if (specialCharacters.includes(l)){
+    isCorrect(l)
+  } else {
+    isWrong(l)
+  }
 }
 
+function isCorrect(l){
+console.log("essa palavra tem a letra " + l)
+}
+
+
+function isWrong(l){
+console.log("essa palavra não tem a letra " + l)
+const newForcaImage = imageForca + 1
+setImageForca(newForcaImage)
+}
   return (
    <div className="containerJogo">
     <Jogo startGame={startGame} imageForca={images[imageForca]} word={word}/>
